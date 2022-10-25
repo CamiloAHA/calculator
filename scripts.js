0c17606ed55bc78d67edb37f9beddb5d4d3bdd2a
CamiloAHA <caa96ha@gmail.com>
2022-10-24 23:15:34 -0500

    Working for negative numbers and screen improve

--- scripts.js
+++ scripts.js
@@ -2,8 +2,75 @@ function display() {
     const screen = document.querySelector('.screen')
     const buttons = document.querySelectorAll('button')
     const dispTxt = document.createElement('h2')
+    const dispResult = document.createElement('h1')
     let numbers, op;
-    dispTxt.innerText = '0'
+    dispTxt.innerText = '0' //Default display is 0
+    dispResult.innerText = ''
+    keySupport() //Keyboard support
+
+    buttons.forEach(btn => btn.addEventListener('click', e => {
+        const btnContent = e.target.innerText
+        const actions = {
+            'AC': () => dispTxt.innerText = '0',                        //Delete key
+            'DEL': () => {                                              //Backspace key
+                dispTxt.innerText = dispTxt.innerText.slice(0, -1)
+                if (dispTxt.innerText === '') dispTxt.innerText = '0'
+            },
+            '=': () => {                                                //Enter key
+                result = txtToResult(dispTxt.innerText, op)
+                if (result != 'error') dispResult.innerText = result
+            }
+        }
+        if (actions[btnContent]) actions[btnContent]()
+        if (/[\d]/.test(btnContent)) { //Numbers
+            (dispTxt.innerText === '0') ? dispTxt.innerText = btnContent : dispTxt.innerText += btnContent
+        } else if (/[.\+\-\/\u00D7]/.test(btnContent)) { //Special characters
+            if (btnContent != '.') {
+                if (/[\+\-\/\u00D7]/.test(dispTxt.innerText.slice(1))) {
+                    result = txtToResult(dispTxt.innerText, op)
+                    if (result != 'error') {
+                        dispResult.innerText = dispTxt.innerText = result
+                        op = btnContent
+                        dispTxt.innerText += btnContent
+                    }
+                } else {
+                    op = btnContent
+                    dispTxt.innerText += btnContent
+                }
+            } else { //For floating points numbers
+                numbers = dispTxt.innerText.slice(1).split(/[\+\-\/\(\)\u00D7]/)
+                //Checking if the current number already has a point
+                if (numbers[1] === undefined && !(/[.]/.test(numbers[0])) ||
+                    numbers[1] != undefined && !(/[.]/.test(numbers[1]))) {
+                    dispTxt.innerText += btnContent
+                }
+            }
+        }
+        e.target.blur() //To avoid conflicts with Enter key
+    }))
+    screen.append(dispTxt)
+    screen.append(dispResult)
+}
+
+function txtToResult(text, op) {
+    let newTxt, result
+    //Fix for negative numbers
+    negative = text.slice(0, 1)
+    negative === '-' ? newTxt = text.slice(1) : newTxt = text
+    //Checking operation signs to avoid evaluation of more than a single pair of numbers at a time.
+    if (/[\+\-\/\u00D7]/.test(newTxt)) {
+        numbers = newTxt.split(op)
+        if (numbers[0] != '' && numbers[1] != '') {
+            if (negative === '-') result = operate(op, +('-' + numbers[0]), +numbers[1])
+            else result = operate(op, +numbers[0], +numbers[1])
+            console.log(text + ' = ' + result)
+            return result
+        }
+    }
+    return 'error' //In case some condition fails
+}
+
+function keySupport() {
     window.addEventListener('keyup', e => {
         if (/^[.\d\+\-\*\/\(\)]/.test(e.key) || e.code === 'Backspace' ||
             e.code === 'Enter' || e.code === 'Delete') {
@@ -15,68 +82,20 @@ function display() {
             pressBtn.click()
         }
     })
-    buttons.forEach(btn => btn.addEventListener('click', e => {
-        const btnContent = e.target.innerText
-        if (btnContent === "AC") { //Delete key
-            dispTxt.innerText = '0'
-        } else if (btnContent === "DEL") { //Backspace key
-            dispTxt.innerText = dispTxt.innerText.slice(0, -1)
-            if (dispTxt.innerText === '') dispTxt.innerText = '0'
-        } else if (btnContent === "=") { //Enter key
-            if (/[\+\-\/\(\)\u00D7]/.test(dispTxt.innerText)) {
-                numbers = dispTxt.innerText.split(/[\+\-\/\(\)\u00D7]/)
-                if (numbers[1] != '') dispTxt.innerText = operate(op, +numbers[0], +numbers[1])
-            }
-        } else if (/[\d\(\)]/.test(btnContent)) { //Numbers and brackets keys
-            (dispTxt.innerText === '0') ? dispTxt.innerText = btnContent : dispTxt.innerText += btnContent
-        } else if (/[.\+\-\/\u00D7]/.test(btnContent)) { //Special characters keys
-            //Checking operation signs to avoid evaluation of more than a single pair of numbers at a time.
-            if (btnContent != '.' && /[\+\-\/\u00D7]/.test(dispTxt.innerText)) {
-                //todo fix for negative numbers
-                numbers = dispTxt.innerText.split(/[\+\-\/\(\)\u00D7]/)
-                if (numbers[0] != '' && numbers[1] != '') {
-                    dispTxt.innerText = operate(op, +numbers[0], +numbers[1])
-                    op = btnContent
-                    dispTxt.innerText += btnContent
-                }
-            } else {
-                if (btnContent != '.') {
-                    op = btnContent
-                    dispTxt.innerText += btnContent
-                } else {
-                    numbers = dispTxt.innerText.split(/[\+\-\/\(\)\u00D7]/)
-                    //Checking if the current number already has a point
-                    if (numbers[1] === undefined && !(/[.]/.test(numbers[0])) ||
-                        numbers[1] != undefined && !(/[.]/.test(numbers[1]))) {
-                        dispTxt.innerText += btnContent
-                    }
-                }
-            }
-        }
-        //To avoid conflicts with Enter key
-        e.target.blur()
-    }))
-    screen.append(dispTxt)
 }
 
 function operate(op, a, b) {
-    if (op === '+') {
-        return +(a + b).toFixed(3)
+    const operations = {
+        '+': +(a + b).toFixed(3),
+        '-': +(a - b).toFixed(3),
+        '\u00D7': +(a * b).toFixed(3),
+        '/': +(a / b).toFixed(3)
     }
-    if (op === '-') {
-        return +(a - b).toFixed(3)
+    if (op === '/' && b === 0) {
+        alert("Forbidden to divide by zero")
+        return a + op + b
     }
-    if (op === '\u00D7') {
-        return +(a * b).toFixed(3)
-    }
-    if (op === '/') {
-        if (b === 0) {
-            alert("Forbidden to divide by zero")
-            return a + op + b
-        }
-        return +(a / b).toFixed(3)
-    }
-    return 'error'
+    return operations[op]
 }
 
 display()
